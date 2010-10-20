e0c59c4cf755e0f55be1a3a4d06b27d7b6e4a135
Oscar Key <oscar@keyfamily.org.uk>
2010-10-20 21:27:50 +0100

    Merge problems

--- src/com/bbcnewsreader/ReaderActivity.java
+++ src/com/bbcnewsreader/ReaderActivity.java
@@ -2,12 +2,17 @@ package com.bbcnewsreader;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class ReaderActivity extends Activity {
+	/** variables */
+	ScrollView scroller;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //scroller = (ScrollView)findViewById("news_scroller");
     }
 }
