440967aff78275a772015e7bd9b724a95933e36a
Thomas <Thomas@SideshowBob>
2010-10-30 21:38:51 +0100

    Setenabledbooleans added

--- src/com/bbcnewsreader/ReaderActivity.java
+++ src/com/bbcnewsreader/ReaderActivity.java
@@ -21,9 +21,11 @@ public class ReaderActivity extends Activity {
         dh.insertItem("Title1", "description1", "link1", "Sat, 30 Oct 2010 09:26:07 GMT", "World");
         dh.insertItem("Title2", "description2", "link2", "Sat, 30 Oct 2010 08:35:00 GMT", "World");
         dh.insertItem("Title3", "description2", "link2", "Sat, 29 Sep 2010 11:01:19 GMT", "World");
-        dh.insertCategory("World",true,"http://feeds.bbci.co.uk/world/rss.xml");
-        dh.insertCategory("Technology",false,"http://feeds.bbci.co.uk/news/rss.xml");
-        dh.insertCategory("Science",true,"http://feeds.bbci.co.uk/science/rss.xml");
+        dh.addCategories();
+        dh.setEnabledCategories(new boolean[] {true,true,false,true,false,false,false,true,true,true,false,true,false});
+        //dh.insertCategory("World",true,"http://feeds.bbci.co.uk/world/rss.xml");
+        //dh.insertCategory("Technology",false,"http://feeds.bbci.co.uk/news/rss.xml");
+        //dh.insertCategory("Science",true,"http://feeds.bbci.co.uk/science/rss.xml");
         dh.clearOld();
         String[][] categories = dh.getItems("World");
         for(int i=0;i<categories[0].length;i++)
