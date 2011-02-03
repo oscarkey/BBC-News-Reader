6673a9d9334f534ead3821f8c5af4cfb61b4c8c2
Oscar Key <oscar@keyfamily.org.uk>
2011-02-03 21:33:58 +0000

    License now fully applied.

--- src/com/bbcnewsreader/ReaderActivity.java
+++ src/com/bbcnewsreader/ReaderActivity.java
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * BBC News Reader
+ * Released under the BSD License. See README or LICENSE.
+ * Copyright (c) 2011, Digital Lizard (Oscar Key, Thomas Boby)
+ * All rights reserved.
+ ******************************************************************************/
 package com.bbcnewsreader;
 
 
