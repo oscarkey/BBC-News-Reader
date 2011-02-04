e797fdde7f6e3233f1d3f19099a0d30d18fe2f5a
Oscar Key <oscar@keyfamily.org.uk>
2011-02-04 23:29:44 +0000

    Added queuing and comparison.

--- src/com/digitallizard/bbcnewsreader/resource/web/QueueItem.java
+++ src/com/digitallizard/bbcnewsreader/resource/web/QueueItem.java
@@ -1,15 +1,27 @@
 package com.digitallizard.bbcnewsreader.resource.web;
 
-public class QueueItem {
+public class QueueItem implements Comparable<QueueItem> {
 	private String url;
 	private int type;
 	private int itemId;
+	private int priority;
 	
-	public QueueItem(String url, int type, int itemId, int priority)
+	public QueueItem(String url, int type, int itemId)
 	{
 		this.url = url;
 		this.type = type;
 		this.itemId = itemId;
+		this.priority = type;
+	}
+	
+	public int compareTo(QueueItem item){
+		if(this.priority > item.priority)
+			return 1;
+		if(this.priority < item.priority)
+			return -1;
+		if(this.priority == item.priority)
+			return 0;
+		return 0; //FIXME this is unneeded
 	}
 	
 	public int getType()
--- src/com/digitallizard/bbcnewsreader/resource/web/WebManager.java
+++ src/com/digitallizard/bbcnewsreader/resource/web/WebManager.java
@@ -7,69 +7,128 @@ import com.digitallizard.bbcnewsreader.ResourceInterface;
 
 import android.util.Log;
 
-public class WebManager {
+public class WebManager implements Runnable {
 	/* constants */
-	static final int ITEM_TYPE_HTML = 0;
+	static final int ITEM_TYPE_HTML = 2;
 	static final int ITEM_TYPE_THUMB = 1;
-	static final int ITEM_TYPE_IMAGE = 2;
+	static final int ITEM_TYPE_IMAGE = 0;
 	
 	/* variables */
 	PriorityQueue<QueueItem> downloadQueue;
-	ResourceInterface resourceInterface;
+	ResourceInterface handler;
 	private boolean queueEmpty;
+	private boolean keepDownloading;
+	Thread downloadThread;
 	
-	private void itemQueued(){
-		if(!queueEmpty){
-			queueEmpty = false;
-			startDownload();
-		}
+
+	synchronized boolean isQueueEmpty() {
+		return queueEmpty;
+	}
+
+	synchronized void setQueueEmpty(boolean queueEmpty) {
+		this.queueEmpty = queueEmpty;
+	}
+
+	synchronized boolean shouldKeepDownloading() {
+		return keepDownloading;
+	}
+
+	synchronized void setKeepDownloading(boolean keepDownloading) {
+		this.keepDownloading = keepDownloading;
 	}
 	
-	private void startDownload(){
-		//FIXME Types need to change to constants
-		QueueItem queueItem;
+	synchronized void setQueue(PriorityQueue<QueueItem> queue){
+		downloadQueue = queue;
+	}
+	
+	synchronized PriorityQueue<QueueItem> getQueue(){
+		return downloadQueue;
+	}
+	
+	private void downloadItem(QueueItem item){
 		while(downloadQueue.size() != 0){
-			queueItem = downloadQueue.poll();
-			switch(queueItem.getType()){
+			item = downloadQueue.poll();
+			switch(item.getType()){
 				case ITEM_TYPE_HTML:
-					downloadHtml(queueItem);
+					downloadHtml(item);
 					break;
 				case ITEM_TYPE_THUMB:
+					downloadThumbnail(item);
 					break;
 				case ITEM_TYPE_IMAGE:
-					downloadImage(queueItem);
+					downloadImage(item);
 					break;
 			}
 		}
 	}
 	
-	void downloadHtml(QueueItem item){
+	private void downloadHtml(QueueItem item){
 		try{
 			URI url = new URI(item.getUrl());
-			String html = null;
-			//html = HtmlParser.getPage(url);
-			resourceInterface.downloadComplete(item.getItemId(), item.getType(), html);
+			String html = HtmlParser.getPage(handler, url); //load the page
+			handler.downloadComplete(item.getItemId(), item.getType(), html);
 		}
 		catch(Exception e){
 			Log.e("htmlparser",e.getMessage());
 		}
 	}
 	
-	void downloadThumbnail(QueueItem item){
+	private void downloadThumbnail(QueueItem item){
 		
 	}
 	
-	void downloadImage(QueueItem item){
+	private void downloadImage(QueueItem item){
 		
+	}
+	
+	private void itemQueued(){
+		//check if we need to start the download thread
+		if(isQueueEmpty()){
+			//start the download thread
+			setQueueEmpty(false);
+			setKeepDownloading(true);
+			downloadThread.start();
+		}
 	}
 	
 	public void addToQueue(String url,int type,int itemId){
-		QueueItem queueItem = new QueueItem(url, type, itemId, -1);
+		QueueItem queueItem = new QueueItem(url, type, itemId);
 		downloadQueue.add(queueItem);
 		itemQueued();
 	}
 	
+	public void emptyQueue(){
+		stopDownload(); //first stop downloading
+		getQueue().clear(); //empty the queue
+	}
+	
+	public void stopDownload(){
+		//try and stop the download
+		setKeepDownloading(false); //this will stop it after the current file
+	}
+	
+	public void run(){
+		//check this hasn't been called in error
+		if(getQueue().size() > 0){
+			//keep downloading if we should
+			while(shouldKeepDownloading()){
+				//retrieve the head of the queue and load it
+				downloadItem(getQueue().poll());
+				//check if the queue is empty now
+				if(getQueue().size() == 0){
+					setQueueEmpty(true); //flag the queue as empty
+					setKeepDownloading(false); //stop the loop
+				}
+			}
+		}
+		else{
+			//as the queue was empty, we should flag it
+			setQueueEmpty(true);
+		}
+	}
+	
 	public WebManager(){
 		downloadQueue = new PriorityQueue<QueueItem>();
+		downloadThread = new Thread(this);
 	}
 }
