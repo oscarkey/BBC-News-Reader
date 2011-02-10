9f2ac143e0b4ee3eb31ca385b0ee8c557d0f56eb
Oscar Key <okey@cc.(none)>
2011-02-10 14:06:08 +0000

    Added new activity with layout xml.

---
+++ src/com/digitallizard/bbcnewsreader/CategoryActivity.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * BBC News Reader
+ * Released under the BSD License. See README or LICENSE.
+ * Copyright (c) 2011, Digital Lizard (Oscar Key, Thomas Boby)
+ * All rights reserved.
+ ******************************************************************************/
+package com.digitallizard.bbcnewsreader;
+
+import android.app.Activity;
+
+public class CategoryActivity extends Activity {
+
+}
