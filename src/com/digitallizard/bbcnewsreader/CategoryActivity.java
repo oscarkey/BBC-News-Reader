8d611e0dbbc78b74e7bc3056fdb66b4c3f751ac4
Oscar Key <oscar@keyfamily.org.uk>
2011-02-11 22:11:33 +0000

    CategoryActivity launching and title setting.

--- src/com/digitallizard/bbcnewsreader/CategoryActivity.java
+++ src/com/digitallizard/bbcnewsreader/CategoryActivity.java
@@ -7,7 +7,16 @@
 package com.digitallizard.bbcnewsreader;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class CategoryActivity extends Activity {
-
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState); //load the superclass
+		this.setContentView(R.layout.category); //load the layout
+		
+		//set the title
+		((TextView)findViewById(R.id.categoryTitle)).setText(this.getIntent().getStringExtra("title"));
+		//LayoutInflater inflater = new LayoutInflater
+	}
 }
