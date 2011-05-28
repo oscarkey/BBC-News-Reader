700b0911a7c7e133ba4c00a022ccf02dfec3baa7
Oscar Key <oscar@keyfamily.org.uk>
2011-05-28 15:38:31 +0100

    Cleaned up after changes in last commit.

--- src/com/digitallizard/bbcnewsreader/CategoryActivity.java
+++ src/com/digitallizard/bbcnewsreader/CategoryActivity.java
@@ -8,22 +8,12 @@ package com.digitallizard.bbcnewsreader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -46,9 +36,7 @@ public class CategoryActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState); //create the parent class
 		this.setContentView(R.layout.category); //load the layout
-		
-		LayoutInflater inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE); //inflater to create layouts
-		
+				
 		listView = (ListView)this.findViewById(R.id.categoryListView);
 		
 		//set the title of this category
@@ -73,56 +61,4 @@ public class CategoryActivity extends Activity {
 			}
 		});
 	}
-	
-	/**public void itemClicked(View item){
-		//create an intent to launch the next activity
-    	Intent intent = new Intent(this, ArticleActivity.class);
-    	TextView titleText = (TextView)item.findViewById(R.id.fullNewsItemName);
-    	intent.putExtra("id", (int)itemIds.get(titleText.getText()));
-    	startActivity(intent);
-	}
-	
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState); //load the superclass
-		this.setContentView(R.layout.category); //load the layout
-		
-		itemIds = new HashMap<String, Integer>();
-		
-		//set the title
-		String title = this.getIntent().getStringExtra("title"); //load title from the intent
-		((TextView)findViewById(R.id.categoryTitle)).setText(title);
-		//create an inflater to build the ui
-		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		LinearLayout scrollerContent = (LinearLayout)findViewById(R.id.categoryScrollerContent);
-		
-		//load in the news items from the database
-		DatabaseHandler database = new DatabaseHandler(this, 0); //we don't need to bother with the clear old date
-		NewsItem[] items = database.getItems(title, 50); //specify a high limit
-		//check if the database was empty
-		if(items != null){
-			//display them
-			for(int i = 0; i < items.length; i++){
-				LinearLayout item = (LinearLayout)inflater.inflate(R.layout.list_full_news_item, null);
-				//set the article name
-				((TextView)item.findViewById(R.id.fullNewsItemName)).setText(items[i].getTitle());
-				((TextView)item.findViewById(R.id.fullNewsItemDescription)).setText(items[i].getDescription());
-				
-				//try and get an image for this item
-				byte[] imageBytes = database.getThumbnail(items[i].getId());
-				//check if any image data was returned
-				if(imageBytes != null){
-					//try to construct an image out of the bytes given by the database
-					Bitmap imageBitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length); //load the image into a bitmap
-					ImageView imageView = (ImageView)item.findViewById(R.id.fullNewsItemImage);
-					imageView.setImageBitmap(imageBitmap);
-				}
-				
-				scrollerContent.addView(item);
-				//save the id for clicks
-				itemIds.put(items[i].getTitle(), items[i].getId());
-			}
-		}
-		database.onDestroy(); //shutdown database
-	}
-	**/
 }
--- src/com/digitallizard/bbcnewsreader/ItemAdapter.java
+++ src/com/digitallizard/bbcnewsreader/ItemAdapter.java
@@ -2,18 +2,17 @@ package com.digitallizard.bbcnewsreader;
 
 import java.util.ArrayList;
 
-import com.digitallizard.bbcnewsreader.data.DatabaseHandler;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.digitallizard.bbcnewsreader.data.DatabaseHandler;
 
 public class ItemAdapter extends ArrayAdapter<NewsItem> {
 	private ArrayList<NewsItem> items;
