277ed089a247fcc2588f7a41cddfc24c9d27378e
Oscar Key <oscar@keyfamily.org.uk>
2011-06-08 12:21:07 +0100

    Removed uneeded logs and imports.

--- src/com/digitallizard/bbcnewsreader/ItemAdapter.java
+++ src/com/digitallizard/bbcnewsreader/ItemAdapter.java
@@ -3,12 +3,9 @@ package com.digitallizard.bbcnewsreader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import com.digitallizard.bbcnewsreader.data.DatabaseHandler;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,14 +38,12 @@ public class ItemAdapter extends ArrayAdapter<NewsItem> {
 			//set the image to the loaded but no image thumnail
 			ImageView imageView = (ImageView)view.findViewById(R.id.categoryItemImage);
 			imageView.setImageResource(R.drawable.no_thumb);
-			Log.v("ItemAdapter", "It ran!");
 		}
 		else if(thumbnailBytes != null){
 			//try to construct an image out of the bytes given by the database
 			Bitmap imageBitmap = BitmapFactory.decodeByteArray(thumbnailBytes, 0, thumbnailBytes.length); //load the image into a bitmap
 			ImageView imageView = (ImageView)view.findViewById(R.id.categoryItemImage);
 			imageView.setImageBitmap(imageBitmap);
-			Log.v("ItemAdapter", "thumbnailBytes");
 		}
 		else{
 			//set the image to the default thumbnail
