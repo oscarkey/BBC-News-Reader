5bacafd59cfd968f60cc9bfeee53badbd97b5e2b
Oscar Key <oscar@keyfamily.org.uk>
2011-06-23 21:18:16 +0100

    Clears categories before each load to make sure items are ordered properly if one instantaneously disappears from the feed.

--- src/com/digitallizard/bbcnewsreader/ResourceService.java
+++ src/com/digitallizard/bbcnewsreader/ResourceService.java
@@ -227,6 +227,9 @@ public class ResourceService extends Service implements ResourceInterface {
 	 * Called when an RSS feed has loaded
 	 * @param item The item that has been loaded */
 	public synchronized void categoryRssLoaded(RSSItem[] items, String category){
+		// clear the priorities for this category to prevent old items hanging around
+		database.clearPriorities(category);
+		
 		//insert the items into the database
 		for(int i = 0; i < items.length; i++){
 			//check there are some thumbnails
