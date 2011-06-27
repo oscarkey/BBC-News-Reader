d647f32ca0a52dcf3fe1d3477c96a34a9dad7c4f
Oscar Key <oscar@keyfamily.org.uk>
2011-06-27 17:45:29 +0100

    Base of widget.

---
+++ src/com/digitallizard/bbcnewsreader/ReaderWidget.java
@@ -0,0 +1,13 @@
+package com.digitallizard.bbcnewsreader;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class ReaderWidget extends AppWidgetProvider {
+	
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+		
+	}
+	
+}
