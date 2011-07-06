654fded4516373369c8394049b6ab7ae6355721e
Oscar Key <oscar@keyfamily.org.uk>
2011-07-06 19:12:34 +0100

    Multiple widget support. Security error.

--- src/com/digitallizard/bbcnewsreader/widget/ReaderWidget.java
+++ src/com/digitallizard/bbcnewsreader/widget/ReaderWidget.java
@@ -7,6 +7,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.digitallizard.bbcnewsreader.ArticleActivity;
@@ -21,46 +22,58 @@ public class ReaderWidget extends AppWidgetProvider {
 	public static final String DEFAULT_CATEGORY = "Headlines"; // the default category
 	
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        ComponentName widget = new ComponentName(context, ReaderWidget.class);
+        for(int i = 0; i < appWidgetIds.length; i++){
+        	Log.e("widget", "widget: "+manager.getAppWidgetInfo(appWidgetIds[i]).toString());
+        }
+        Log.e("widget", "id length: "+appWidgetManager.getAppWidgetIds(widget).length);
+        
 		// retrieve the news from the database
 		DatabaseHandler database = new DatabaseHandler(context);
 		SharedPreferences settings = context.getSharedPreferences(ReaderActivity.PREFS_FILE_NAME, Context.MODE_PRIVATE);
-		String category = settings.getString(PREF_KEY_CATEGORY, DEFAULT_CATEGORY);
-		NewsItem[] items = database.getItems(category, NUM_ITEMS);
 		
-		// create references to the required view
-		RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget);
-		
-		// make the bbc news logo clickable
-		Intent appIntent = new Intent(context, ReaderActivity.class);
-		PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
-		view.setOnClickPendingIntent(R.id.widgetLogo, appPendingIntent);
-		
-		// remote existing views from the flipper
-		view.removeAllViews(R.id.widgetFlipper);
-		
-		// loop through and add the latest news to the item
-		for(int i = 0; i < NUM_ITEMS && i < items.length; i++){
-			// create a view for this item
-			RemoteViews item = new RemoteViews(context.getPackageName(), R.layout.widget_item);
-			// set the text
-			item.setTextViewText(R.id.widgetItemTitle, items[i].getTitle());
-			item.setTextViewText(R.id.widgetItemDesc, items[i].getDescription());
+		for(int i = 0; i < appWidgetIds.length; i++){
+			String category = settings.getString(PREF_KEY_CATEGORY + appWidgetIds[i], DEFAULT_CATEGORY);
+			NewsItem[] items = database.getItems(category, NUM_ITEMS);
 			
-			// make the item clickable
-			Intent itemIntent = new Intent(context, ArticleActivity.class);
-			itemIntent.putExtra("id", items[i].getId());
-			PendingIntent itemPendingIntent = PendingIntent.getActivity(context, i, itemIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
-			item.setOnClickPendingIntent(R.id.widgetItemTitle, itemPendingIntent);
-			item.setOnClickPendingIntent(R.id.widgetItemDesc, itemPendingIntent);
+			Log.e("widget", "got "+category+" from "+PREF_KEY_CATEGORY + appWidgetIds[i]);
 			
-			// add this item to the flipper
-			view.addView(R.id.widgetFlipper, item);
+			// create references to the required view
+			RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget);
+			
+			// make the bbc news logo clickable
+			Intent appIntent = new Intent(context, ReaderActivity.class);
+			PendingIntent appPendingIntent = PendingIntent.getActivity(context, 0, appIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+			view.setOnClickPendingIntent(R.id.widgetLogo, appPendingIntent);
+			
+			// remote existing views from the flipper
+			view.removeAllViews(R.id.widgetFlipper);
+			
+			// loop through and add the latest news to the item
+			for(int j = 0; j < NUM_ITEMS && j < items.length; j++){
+				// create a view for this item
+				RemoteViews item = new RemoteViews(context.getPackageName(), R.layout.widget_item);
+				// set the text
+				item.setTextViewText(R.id.widgetItemTitle, items[j].getTitle());
+				item.setTextViewText(R.id.widgetItemDesc, items[j].getDescription());
+				
+				// make the item clickable
+				Intent itemIntent = new Intent(context, ArticleActivity.class);
+				itemIntent.putExtra("id", items[j].getId());
+				PendingIntent itemPendingIntent = PendingIntent.getActivity(context, j, itemIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+				item.setOnClickPendingIntent(R.id.widgetItemTitle, itemPendingIntent);
+				item.setOnClickPendingIntent(R.id.widgetItemDesc, itemPendingIntent);
+				
+				// add this item to the flipper
+				view.addView(R.id.widgetFlipper, item);
+			}
+			
+			// update the widget with the updated views
+			//ComponentName widget = new ComponentName(context, ReaderWidget.class);
+	        manager.updateAppWidget(appWidgetIds[i], view);
+	        //manager.updateAppWidget(widget, view);
 		}
-		
-		// update the widget with the updated views
-		ComponentName widget = new ComponentName(context, ReaderWidget.class);
-        AppWidgetManager manager = AppWidgetManager.getInstance(context);
-        manager.updateAppWidget(widget, view);
 	}
 	
 }
--- src/com/digitallizard/bbcnewsreader/widget/WidgetConfigActivity.java
+++ src/com/digitallizard/bbcnewsreader/widget/WidgetConfigActivity.java
@@ -1,10 +1,16 @@
 package com.digitallizard.bbcnewsreader.widget;
 
 import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.digitallizard.bbcnewsreader.R;
@@ -13,18 +19,18 @@ import com.digitallizard.bbcnewsreader.data.DatabaseHandler;
 
 public class WidgetConfigActivity extends Activity {
 	ListView listView;
-	
-	public void saveClicked(View view){
-		
-	}
+	String[] enabledCategories;
+	SharedPreferences settings;
+	int widgetId;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.widget_config);
+		widgetId = getIntent().getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
 		
 		// connect to the database and retrieve the enabled categories
 		DatabaseHandler database = new DatabaseHandler(this);
-		String[] enabledCategories = database.getEnabledCategories()[1];
+		enabledCategories = database.getEnabledCategories()[1];
 		
 		// set up the list view
 		listView = (ListView)this.findViewById(R.id.widgetCategoryChoiceListView);
@@ -32,8 +38,8 @@ public class WidgetConfigActivity extends Activity {
 		listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 		
 		// find the id of the current item and enable it
-		SharedPreferences settings = getSharedPreferences(ReaderActivity.PREFS_FILE_NAME, MODE_PRIVATE);
-		String enabledCategory = settings.getString(ReaderWidget.PREF_KEY_CATEGORY, ReaderWidget.DEFAULT_CATEGORY);
+		settings = getSharedPreferences(ReaderActivity.PREFS_FILE_NAME, MODE_PRIVATE);
+		String enabledCategory = settings.getString(ReaderWidget.PREF_KEY_CATEGORY + widgetId, ReaderWidget.DEFAULT_CATEGORY);
 		int enabledCategoryId = 0;
 		for(int i = 0; i < enabledCategories.length; i++){
 			if(enabledCategories[i].equals(enabledCategory)){
@@ -41,5 +47,34 @@ public class WidgetConfigActivity extends Activity {
 			}
 		}
 		listView.setItemChecked(enabledCategoryId, true);
+		
+		// if the activity is cancelled
+		Intent cancelResult = new Intent();
+		cancelResult.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+		setResult(RESULT_CANCELED, cancelResult);
+		
+		// add a trigger to the save button to save the selected category
+		Button saveButton = (Button)this.findViewById(R.id.widgetChoiceSaveButton);
+		saveButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View view){
+				// store the selected category
+				String chosenCategory = enabledCategories[(int)listView.getCheckedItemPosition()];
+				Editor editor = settings.edit();
+				Log.v("widget config", "putting "+chosenCategory+" into "+ReaderWidget.PREF_KEY_CATEGORY + widgetId);
+				editor.putString(ReaderWidget.PREF_KEY_CATEGORY + widgetId, chosenCategory);
+				
+				// update the widget
+				Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+				int[] widgetIds = {widgetId};
+				intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetIds);
+				sendBroadcast(intent);
+				
+				// send a successful result
+				Intent successResult = new Intent();
+				successResult.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+				setResult(RESULT_OK, successResult);
+				finish(); // end the activity and send the result
+			}
+		});
 	}
 }
